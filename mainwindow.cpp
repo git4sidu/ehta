793a291b79c6ab82c5e1e0aad198273b799c020b
mark <fake.book@o2.pl>
2017-07-03 14:45:34 +0200

    udp send working

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,81 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "udpsend.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    socket = new QUdpSocket(this);
+    socket->bind(QHostAddress::LocalHost, 29999);
+    connect(socket, SIGNAL(readyRead()), this, SLOT (MyRead()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_Slider_x_valueChanged(int value)
+{
+    QByteArray Data;
+    Data.append("A");
+    Data.append(ui->Slider_x->value());
+    socket->writeDatagram(Data, QHostAddress(ui->textEdit_IP->toPlainText()), (ui->textEdit_Port->toPlainText()).toInt());
+}
+
+void MainWindow::on_Slider_y_valueChanged(int value)
+{
+    QByteArray Data;
+    Data.append("B");
+    Data.append(ui->Slider_y->value());
+    socket->writeDatagram(Data, QHostAddress(ui->textEdit_IP->toPlainText()), (ui->textEdit_Port->toPlainText()).toInt());
+}
+
+void MainWindow::on_Slider_z_valueChanged(int value)
+{
+    QByteArray Data;
+    Data.append("C");
+    Data.append(ui->Slider_z->value());
+    socket->writeDatagram(Data, QHostAddress(ui->textEdit_IP->toPlainText()), (ui->textEdit_Port->toPlainText()).toInt());
+}
+
+void MainWindow::on_Slider_x_rot_valueChanged(int value)
+{
+    QByteArray Data;
+    Data.append("D");
+    Data.append(ui->Slider_x_rot->value());
+    socket->writeDatagram(Data, QHostAddress(ui->textEdit_IP->toPlainText()), (ui->textEdit_Port->toPlainText()).toInt());
+}
+
+void MainWindow::on_Slider_y_rot_valueChanged(int value)
+{
+    QByteArray Data;
+    Data.append("E");
+    Data.append(ui->Slider_y_rot->value());
+    socket->writeDatagram(Data, QHostAddress(ui->textEdit_IP->toPlainText()), (ui->textEdit_Port->toPlainText()).toInt());
+}
+
+void MainWindow::on_Slider_z_rot_valueChanged(int value)
+{
+    QByteArray Data;
+    Data.append("F");
+    Data.append(ui->Slider_z_rot->value());
+    socket->writeDatagram(Data, QHostAddress(ui->textEdit_IP->toPlainText()), (ui->textEdit_Port->toPlainText()).toInt());
+}
+
+void MainWindow::JustSend()
+{
+    QByteArray Data;
+    Data.append(MainWindow::ui->textEdit->toPlainText());
+    socket->writeDatagram(Data, QHostAddress(ui->textEdit_IP->toPlainText()), (ui->textEdit_Port->toPlainText()).toInt());
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    MainWindow::JustSend();
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+
 #include <QMainWindow>
+#include "udpsend.h"
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +16,26 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+         void JustSend();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_Slider_x_valueChanged(int value);
+
+    void on_Slider_y_valueChanged(int value);
+
+    void on_Slider_z_valueChanged(int value);
+
+    void on_Slider_x_rot_valueChanged(int value);
+
+    void on_Slider_y_rot_valueChanged(int value);
+
+    void on_Slider_z_rot_valueChanged(int value);
 private:
     Ui::MainWindow *ui;
+    QUdpSocket *socket;
+
 };
 
 #endif // MAINWINDOW_H
